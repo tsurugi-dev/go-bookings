f1c875dab312a335940b6a497a26856056f645e3
tsurugi-dev <dev.tsurugi-dev@gmail.com>
2022-02-15 23:10:11 +0900

    応答データはファイルから読み込むようにする

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	render(w, "home.page.tmpl")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	render(w, "about.page.tmpl")
+}
+
+func render(w http.ResponseWriter, name string) {
+	t, err := template.ParseFiles("./templates/" + name)
+	if err != nil {
+		log.Fatal("テンプレートファイルが正しく読み取りません")
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Fatal("テンプレートファイルレンダリング失敗しました。")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprint(w, "Hello golang!")
-		if err != nil {
-			log.Println("レスポンス書き込みエラー", err)
-		}
-		log.Printf("レスポンス書き込みサイズ: %d\n", n)
-	})
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
